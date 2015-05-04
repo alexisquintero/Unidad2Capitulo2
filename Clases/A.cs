8e3940c74f5719b2cc82369457c089deb806a26b
emacastro <crzemanuelc@gmail.com>
2015-05-04 20:17:54 -0300

    1.5

---
+++ Clases/A.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Console; esta mal
+
+namespace Clases
+{
+    public class A
+    {
+        private string NombreInstancia;
+
+        public A(){
+            NombreInstancia = "Instancia sin nombre";
+        }
+
+        public A(string nombre)
+        {
+            NombreInstancia = nombre;
+        }
+
+        public void MostrarNombre(){
+            
+            Console.WriteLine(NombreInstancia);           
+        }
+
+        public void M1(){
+            Console.WriteLine("El método M1 fue invocado");
+        }
+        public void M2()
+        {
+            Console.WriteLine("El método M2 fue invocado");
+        }
+
+        public void M3()
+        {
+            Console.WriteLine("El método M3 fue invocado");
+        }
+    }
+
+}
---
+++ Clases/B.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clases
+{
+    public class B : A 
+    {
+        public void M4()
+        {
+            Console.WriteLine("Método del hijo Invocado");
+        }
+
+        public B():base("Instancia de B")
+        {        
+        }
+    }
+}
